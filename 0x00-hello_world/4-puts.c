470966e28d7f750a2ab502a0c17ce2becf538a5d
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2023-08-11 06:04:52 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
-*main - A program that prints a line with puts functio
-n
-*Return: 0 (Success)
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
 */
+
 int main(void)
 {
-puts("\"programming is like building a multilingual puzz
-le");
-return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
