121142e10a53a98b1af1dfc6013df72f20d1525a
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2023-10-15 05:31:06 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,12 +11,12 @@ int term2 = 2;
 int next;
 int i;
 
-printf("%d, ", term2);
+printf("%d, ", term1);
 for (i + 1 ; i < 50 ; i++)
 {
 printf("%d", term2);
-next = term2 + term2;
-term2 = term2;
+next = term1 + term2;
+term1 = term2;
 term2 = next;
 }
 printf("\n");
