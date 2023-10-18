f28f4053d8401b6768420a3f73ec71505709ad82
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2023-10-17 20:13:29 -0700

    A

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,16 +4,18 @@
  * puts_half - print second half of a string
  * @str: char array string type
  * Description: if odd number of chars, prints (length - 1) /2
- */
+*/
 
 void puts_half(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i++);
+for (i = 0; str[i] != '\0'; i++)
+;
 i++;
 for (i /= 2; str[i] != '\0'; i++)
 {
 putchar(str[i]);
 }
+
 putchar('\n');
 }
