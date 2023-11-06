6b7ed1ec851aef09ef43661f1763a1dda1f10393
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2023-11-05 23:03:47 -0800

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,11 +5,12 @@
 #define ERR_MSG "Error"
 
 /**
- * is_digit - check if a string contains a non-digit char
+ * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
- * Return: 0 if a nondigit is found and 1 if digit is found
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
 */
-int is_digit(char *s)
+int is_digit(char*s)
 {
 int i = 0;
 while (s[i])
@@ -24,7 +25,8 @@ return (1);
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- * Return: length of the string
+ *
+ * Return:the length of the string
 */
 int _strlen(char *s)
 {
@@ -45,10 +47,11 @@ exit(98);
 }
 
 /**
- * main - multiply two positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
 */
 int main(int argc, char *argv[])
 {
@@ -67,9 +70,9 @@ for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 {
-digit1 = s1[len] - '0';
+digit1 = s1[len1] - '0';
 carry = 0;
-for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+for (len2 = _strlen(s2) - 1;len2 >= 0; len2--)
 {
 digit2 = s2[len2] - '0';
 carry += result[len1 + len2 + 1] + (digit1 *digit2);
