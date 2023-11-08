7f9027ff2776bf068b86ba439547ac2a0905f873
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2023-11-08 01:39:38 -0800

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,14 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog - a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
+ * struct dog - a struct that create a dog info
  *
- * Description: just a lone dog struct in a big kitty world
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: first struct with alx
 */
 struct dog
 {
@@ -15,5 +16,12 @@ char *name;
 float age;
 char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
