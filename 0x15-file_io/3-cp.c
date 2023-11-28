d7364c0e78e9ef0613657f892967fa4de4372f92
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2023-11-28 05:36:37 -0800

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -58,6 +58,7 @@ dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
 exit(100);
 }
 err_close = close(file_to);
+if (err_close == -1)
 {
 dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
 exit(100);
