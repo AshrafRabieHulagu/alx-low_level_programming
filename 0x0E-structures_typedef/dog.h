3070506238a526ae0a863c6ef27af1898e14984a
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2023-12-29 03:40:00 -0800

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,17 @@
-#ifndef DOG
-#define DOG
+#ifndef _HEADER_
+#define _HEADER_
 
 
 /**
-* struct dog - dog class
-*
-* @name: Name of the dog
-*
-* @age: Age of the dog
-*
-* @owner: Owner of the dog
+ * struct dog - Dog attributes
+ *
+ * @name: The name of the dog.
+ *
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
 */
-
-
 struct dog
 {
 char *name;
@@ -21,7 +20,7 @@ char *owner;
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for dog structure
 */
 typedef struct dog dog_t;
 
